8cda2df7b35faefa26a0b36ddc6dee1ec577ec1d
alphastreet2026 <kaanalp@live.com>
2026-08-26 00:40:23 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+   public:
+    bool isPalindrome(string s) {
+        // std::ranges::transform(s, s.begin(), [](unsigned char c) { return std::isalnum(c); });
+        // auto view = s | std::views::filter([](unsigned char c) { return std::isalnum(c); });
+        std::transform(s.begin(), s.end(), s.begin(),
+                       [](unsigned char c) { return std::toupper(c); });
+        int lo = 0;
+        int hi = s.size()-1;
+        cout << "S=" << s << endl;
+        while (lo < hi) {
+            if (!std::isalnum(s[lo])){
+                ++lo;
+                continue;
+            }
+            if (!std::isalnum(s[hi])){
+                --hi;
+                continue;
+            }
+            if (s[lo++] != s[hi--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
