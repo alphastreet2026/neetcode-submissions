aca8b4736f5355faf9059bf4dbb645a0cfa510f4
alphastreet2026 <kaanalp@live.com>
2026-08-29 17:27:38 -0400

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+    unordered_map<Node*, Node*> map;
+
+    Node* clone(Node* node) {
+        if (!node){
+            return nullptr;
+        }
+        if (map.contains(node)) {
+            return map[node];
+        }
+
+        Node* copyNode = new Node(node->val);
+        map[node] = copyNode;
+
+        for (Node* adj : node->neighbors) {
+            auto copyAdj = clone(adj);
+
+            copyNode->neighbors.push_back(copyAdj);
+        }
+        return copyNode;
+    }
+
+   public:
+    Node* cloneGraph(Node* node) { return clone(node); }
+};
