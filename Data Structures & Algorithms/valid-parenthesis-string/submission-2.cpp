bdd7320048eb28e2dcfffef341c0c19a90470fdc
alphastreet2026 <kaanalp@live.com>
2026-09-14 15:53:34 -0400

    Add: valid-parenthesis-string - submission-2

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+   public:
+    bool checkValidString(string s) {
+        stack<int> lp;
+        stack<int> w;
+
+        for (int i = 0; i < s.size(); ++i) {
+            char c = s[i];
+            if (c == '(') {
+                lp.push(i);
+            } else if (c == '*') {
+                w.push(i);
+            } else if (c == ')') {
+                if (!lp.empty()) {
+                    lp.pop();
+                } else if (!w.empty()) {
+                    w.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        while (!lp.empty()) {
+            int pId = lp.top();
+            lp.pop();
+            if (w.empty() || w.top() < pId) {
+                return false;
+            }
+            w.pop();
+        }
+
+        return true;
+    }
+};
